4fc02978b4be89da69b53ec29131a6dca0d49911
Cesar1606 <167567227+Cesar1606@users.noreply.github.com>
2025-08-06 20:37:27 -0500

    Actualizar presentaciones de Castrol CRB Multi CK-4 15W-40: 6x1L, 3x4L, 2.5GL, 20L

--- script.js
+++ script.js
@@ -94,8 +94,10 @@ const products = {
             viscosity: '15W-40', 
             oilType: 'Semi-sintético', 
             formats: [
-                { size: '18.9 L', image: 'img/Castrol CRB Multi CK-4 15W-40 - 18.9 L.jpg' },
-                { size: '5 gal', image: 'img/Castrol CRB Multi CK-4 15W-40 - 5gal.jpg' }
+                { size: '6x1L', image: 'img/Castrol CRB Multi CK-4 15W-40 - 6x1L.jpg' },
+                { size: '3x4L', image: 'img/Castrol CRB Multi CK-4 15W-40 - 3x4L.jpg' },
+                { size: '2.5GL', image: 'img/Castrol CRB Multi CK-4 15W-40 - 2.5GL.jpg' },
+                { size: '20L', image: 'img/Castrol CRB Multi CK-4 15W-40 - 20L.jpg' }
             ],
             description: 'Aceite semi-sintético para motores diésel, excelente limpieza y protección.', 
             category: 'diesel', 
